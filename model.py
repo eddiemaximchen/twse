38d97f96a5b9af01454123f45a07963f49e15114
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-02-10 23:50:46 +0800

    Update model.py

--- model.py
+++ model.py
@@ -12,48 +12,55 @@ allstock=15140
 
 #取出本益比
 files=[]
-ratio={}
-with open('files/201101yield.csv',encoding='cp950') as file:
-    csvReader=csv.DictReader(file)
+ratio=[]
+file=open('files/201101yield.csv','r',encoding='cp950')
+lines=file.readlines()
+file.close()
+list1=[]
+list2=[]
+list3=[]
+#get 本益比
+sum_pe=0
+for i in lines:
+    list1=i.split(',')
+    list2.append(list1[4])
+    list3.append(list1[5])
+del list2[0]
+del list3[0]
+pe=[float(list) for list in list2] #從字串改成浮點數
+div=[float(list) for list in list3]
+for i in pe:
+    sum_pe=sum_pe+i
+avg_pe=sum_pe/allstock
 
-    for row in csvReader:
-        files.append(row)
+sum_div=0
+#get 殖利率 
+for i in div:
+    sum_div=sum_div+i
+avg_div=sum_div/allstock
+#留下嘗試紀錄
+# with open('files/201101yield.csv',encoding='cp950') as file:
+    # csvReader=csv.DictReader(file)
+# 
+    # every record is a dictory
+    # for row in csvReader:
+        # ratio.append({
+            # '日期':row['日期'],
+            # '證券代號':row['證券代號'],
+            # '本益比':row['本益比'],
+            # '殖利率':row['殖利率']
+# 
+        # })
+# for i in range(15140):
+    # with open("files/json/ration.json", "w", encoding="utf-8") as file:
+        # file.write(json.dumps(ratio, ensure_ascii=False, indent=4))
 #字典值都是字串 所以再塞入list 數字才能轉成int *一定要to_dict()才有效*
 #to_dict()取出的值是錯的
-ratio=pd.DataFrame(files).to_dict()
-pe=[]
-sum_pe=0
-#取出本益比
-for i in ratio['本益比']:
-    pe.append(i)
-for i in pe:
-    sum_pe=i+sum_pe
-avg_pe=sum_pe/(allstock*stock_open)
-div=[]
-sum_div=0
-#取出殖利率
-for i in ratio['殖利率']:
-    div.append(i)
-for i in div:
-    sum_div=i+sum_div
-avg_div=sum_div/(allstock*stock_open)
+#sum(.values)==>TypeError: unsupported operand type(s) for +: 'int' and 'str'
+#ratio=pd.DataFrame(files).to_json(force_ascii=False)值亂了
+#兩層中括弧去不掉
+# ratio=pd.DataFrame(files).values.tolist()
 
-# 取出成交量
-files1=[]
-vol=[]
-with open('files/market/FMTQIK_201101.csv',encoding='cp950') as file1:
-    csvReader=csv.DictReader(file1)
-    for row in csvReader:
-        files1.append(row)
-vol_dict=pd.DataFrame(files1).to_dict()
-vo1=[]
-sum_pe=0
-for i in vol_dict['成交股數']:
-    vol.append(i)
-sum_vol=0
-for i in vol:
-    sum_vol=i+sum_vol
-avg_vol=sum_vol/stock_open
 #載入個股資料
 stock1301=pd.read_json('files/json/stock1301.json')
 stock1303=pd.read_json('files/json/stock1303.json')
@@ -61,7 +68,6 @@ stock2330=pd.read_json('files/json/stock2330.json')
 stock2882=pd.read_json('files/json/stock2882.json')
 stock3008=pd.read_json('files/json/stock3008.json')
 #算個股資料
-stock_result=[]
 
 sum_div_1301=0
 sum_deal_num_1301=0
@@ -117,45 +123,25 @@ for i in stock3008:
 avg_div_3008=sum_div_3008/20
 avg_deal_num_3008=sum_deal_num_3008/20
 avg_pe_3008=sum_pe_3008/20
-# 挑出殖利率大於平均的2倍 本益率大於平均的2倍 成交量大於平均成交量 
-if avg_div_1301 > (avg_div) and avg_pe_1301 >(avg_pe) and avg_deal_num_1301 >avg_vol:
-    stock_result.append({
-        'stockNo':'1301',
-        'avg_div':avg_div_1301,
-        'avg_deal_num':avg_deal_num_1301
-    })
 
-if avg_div_1303 > (avg_div) and avg_pe_1303 >(avg_pe) and avg_deal_num_1303 >avg_vol:
-    stock_result.append({
-        'stockNo':'1303',
-        'avg_div':avg_div_1303,
-        'avg_deal_num':avg_deal_num_1303
-    })
-if avg_div_2330 > (avg_div) and avg_pe_2330 >(avg_pe) and avg_deal_num_2330 >avg_vol:
-    stock_result.append({
-        'stockNo':'2330',
-        'avg_div':avg_div_2330,
-        'avg_deal_num':avg_deal_num_2330
-    })
+# 挑出殖利率大於平均 本益比大於平均 
+stock_result=''
+if avg_div_1301 > (avg_div) and avg_pe_1301 >(avg_pe):
+    stock_result=stock_result+'1301,'
 
-if avg_div_2882 > (avg_div) and avg_pe_2882 >(avg_pe) and avg_deal_num_2882 >avg_vol:
-    stock_result.append({
-        'stockNo':'2882',
-        'avg_div':avg_div_2882,
-        'avg_deal_num':avg_deal_num_2882
-    })
+if avg_div_1303 > (avg_div) and avg_pe_1303 >(avg_pe):
+    stock_result=stock_result+'1303,'
+if avg_div_2330 > (avg_div) and avg_pe_2330 >(avg_pe):
+    stock_result=stock_result+'2330,'
 
-if avg_div_3008 > (avg_div) and avg_pe_3008 >(avg_pe) and avg_deal_num_3008 >avg_vol:
-    stock_result.append({
-        'stockNo':'3008',
-        'avg_div':avg_div_3008,
-        'avg_deal_num':avg_deal_num_3008
-    })
+if avg_div_2882 > (avg_div) and avg_pe_2882 >(avg_pe):
+    stock_result=stock_result+'2882,'
+
+if avg_div_3008 > (avg_div) and avg_pe_3008 >(avg_pe):
+    stock_result=stock_result+'3008,'
 
 #發送到line
-msg="推薦個股:"
-for i in stock_result:
-    msg=msg+i
+msg='推薦個股'+stock_result+'殖利率&本益比均高於市場平均'
 
 
 headers={
